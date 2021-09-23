5db11ddbc1eeba8cbc6655000cf4f255c27a1a22
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-09-24 00:35:56 +0300

    create posts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_one_attached :avatar
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
